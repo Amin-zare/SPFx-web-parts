e0cd019f24c0068d0db41e35d0fc4a51ef865b22
Amin-zare <zare.mohammadamin@gmail.com>
2023-10-15 09:37:23 +0200

    Initial commit FC

--- src/webparts/template/components/Template.tsx
+++ src/webparts/template/components/Template.tsx
@@ -3,37 +3,36 @@ import styles from "./Template.module.scss";
 import type { ITemplateProps } from "./ITemplateProps";
 import { escape } from "@microsoft/sp-lodash-subset";
 
-export default class Template extends React.Component<ITemplateProps, {}> {
-  public render(): React.ReactElement<ITemplateProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName,
-    } = this.props;
+const Template: React.FC<ITemplateProps> = (props: ITemplateProps) => {
+  const {
+    description,
+    isDarkTheme,
+    environmentMessage,
+    hasTeamsContext,
+    userDisplayName,
+  } = props;
 
-    return (
-      <section
-        className={`${styles.template} ${hasTeamsContext ? styles.teams : ""}`}
-      >
-        <div className={styles.welcome}>
-          <img
-            alt=""
-            src={
-              isDarkTheme
-                ? require("../assets/welcome-dark.png")
-                : require("../assets/welcome-light.png")
-            }
-            className={styles.welcomeImage}
-          />
-          <h2> User name: {escape(userDisplayName)}!</h2>
-          <div> Environment Message :{environmentMessage}</div>
-          <div>
-            Description : <strong>{escape(description)}</strong>
-          </div>
+  return (
+    <section
+      className={`${styles.template} ${hasTeamsContext ? styles.teams : ""}`}
+    >
+      <div className={styles.welcome}>
+        <img
+          alt=""
+          src={
+            isDarkTheme
+              ? require("../assets/welcome-dark.png")
+              : require("../assets/welcome-light.png")
+          }
+          className={styles.welcomeImage}
+        />
+        <h2> User name: {escape(userDisplayName)}!</h2>
+        <div> Environment Message :{environmentMessage}</div>
+        <div>
+          Description : <strong>{escape(description)}</strong>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+export default Template;
